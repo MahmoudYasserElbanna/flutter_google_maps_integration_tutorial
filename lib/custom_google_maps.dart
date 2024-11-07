befe6991bbd42c9f0db996aff263623d206c7824
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-11-07 14:06:58 +0200

    add maps markers

--- lib/custom_google_maps.dart
+++ lib/custom_google_maps.dart
@@ -12,10 +12,12 @@ class CustomGoogleMaps extends StatefulWidget {
 
 class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
   late CameraPosition initialCameraPosition;
+  late LatLng initialLatLng =
+      const LatLng(30.11755738457697, 31.229452764126194);
   @override
   void initState() {
-    initialCameraPosition = const CameraPosition(
-      target: LatLng(30.116963436017798, 31.226191198126696),
+    initialCameraPosition = CameraPosition(
+      target: initialLatLng,
       zoom: cityZoomView,
     );
     super.initState();
@@ -25,6 +27,29 @@ class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
   Widget build(BuildContext context) {
     return GoogleMap(
       initialCameraPosition: initialCameraPosition,
+      mapType: MapType.hybrid,
+      markers: {
+        Marker(
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+          markerId: const MarkerId('1'),
+          position: initialLatLng,
+        ),
+        Marker(
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
+          markerId: const MarkerId('2'),
+          position: const LatLng(30.10003440083548, 31.230139409599772),
+        ),
+      },
     );
   }
 }
+
+/*
+  - Google Map Types (Whole Change + Supported from Google)
+  1- hybrid
+  2- terrain
+  3- satellite
+  4- normal
+  5- none
+ */
