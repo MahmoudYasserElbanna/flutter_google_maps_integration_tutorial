21b38376053deae3c37a91611863a156cf9d96b1
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-11-07 16:35:22 +0200

    un display maps API keys

--- lib/custom_google_maps.dart
+++ lib/custom_google_maps.dart
@@ -14,6 +14,8 @@ class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
   late CameraPosition initialCameraPosition;
   late LatLng initialLatLng =
       const LatLng(30.11755738457697, 31.229452764126194);
+  late GoogleMapController googleMapController;
+
   @override
   void initState() {
     initialCameraPosition = CameraPosition(
@@ -24,23 +26,52 @@ class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
   }
 
   @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: initialCameraPosition,
-      mapType: MapType.hybrid,
-      markers: {
-        Marker(
-          icon:
-              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
-          markerId: const MarkerId('1'),
-          position: initialLatLng,
+    return Stack(
+      children: [
+        GoogleMap(
+          initialCameraPosition: initialCameraPosition,
+          onMapCreated: (controller) => {
+            googleMapController = controller,
+          },
+          mapType: MapType.hybrid,
+          markers: {
+            Marker(
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueAzure),
+              markerId: const MarkerId('1'),
+              position: initialLatLng,
+            ),
+            Marker(
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueCyan),
+              markerId: const MarkerId('2'),
+              position: const LatLng(30.10003440083548, 31.230139409599772),
+            ),
+          },
         ),
-        Marker(
-          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
-          markerId: const MarkerId('2'),
-          position: const LatLng(30.10003440083548, 31.230139409599772),
-        ),
-      },
+        Positioned(
+          bottom: 16,
+          right: 16,
+          left: 16,
+          child: ElevatedButton(
+            onPressed: () {
+              googleMapController.animateCamera(
+                CameraUpdate.newLatLng(
+                  const LatLng(30.79641090743415, 30.814353895476902),
+                ),
+              );
+            },
+            child: const Text('Search'),
+          ),
+        )
+      ],
     );
   }
 }
