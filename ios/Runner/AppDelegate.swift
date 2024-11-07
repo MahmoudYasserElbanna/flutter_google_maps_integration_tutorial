9cc38852eaf8105c164ea656adc9d8d7936bd1c6
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-11-07 13:29:56 +0200

    display google maps successfully

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,13 @@
 import UIKit
 import Flutter
-
+import GoogleMaps
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCdOA2pn3V5vZQU5VtK53lf0wUdwDvr0TE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
